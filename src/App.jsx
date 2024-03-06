716c495d0f4f183e17fee1b6ceeb7fc22702fe1e
Axel Acevedo <axelacevedo021@gmail.com>
2024-03-05 21:25:29 -0300

    main

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import Navbar from "./components/Navbar/Navbar"
 import ItemListContainer from "./components/ItemListContainer/ItemListContainer"
+import MainContent from "./components/MainContent/MainContent"
 
 function App() {
   return (
     <>
     <Navbar />
     <ItemListContainer greeting='Bienvenidos a Play Mania' />
+    <MainContent />
     </>
   )
 }
