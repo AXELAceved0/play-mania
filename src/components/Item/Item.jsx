f2c5750bd30e353f7b5d7c2e00ce696d3d62d495
Axel Acevedo <axelacevedo021@gmail.com>
2024-03-10 21:16:21 -0300

    tarjet

--- src/components/Item/Item.jsx
+++ src/components/Item/Item.jsx
@@ -3,9 +3,9 @@ import classes from './Item.module.css'
 const Item = ({img, nombre, precio}) => {
     return (
         <div className={classes.divContainer}>
-        <img src={img} style={{width:240, height:340}} />
+        <img className={classes.imagenes} src={img} />
         <h2>{nombre}</h2>
-        <h3>Precio: ${precio}</h3>
+        <h3 style={{padding:6}}>Precio: ${precio}</h3>
         <button className={classes.Button}>Agregar al Carrito</button>
     </div>
     )
